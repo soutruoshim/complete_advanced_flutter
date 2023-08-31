892b529c937becb4518dbcb73ede3424cbdb1965
Sout Rahim <sout.rahim@gmail.com>
2023-08-31 21:50:35 +0700

    ErrorHandler

--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -4,6 +4,7 @@ import 'package:dartz/dartz.dart';
 import '../../domain/model.dart';
 import '../../domain/repository.dart';
 import '../data_source/remote_data_source.dart';
+import '../network/error_handler.dart';
 import '../network/failure.dart';
 import '../network/network_info.dart';
 import '../request/request.dart';
@@ -19,22 +20,27 @@ class RepositoryImpl extends Repository {
       LoginRequest loginRequest) async {
     if (await _networkInfo.isConnected) {
       // its safe to call the API
-      final response = await _remoteDataSource.login(loginRequest);
+      try {
+        // its safe to call the API
+        final response = await _remoteDataSource.login(loginRequest);
 
-      if (response.status == 0) // success
-          {
-        // return data (success)
-        // return right
-        return Right(response.toDomain());
-      } else {
-        // return biz logic error
-        // return left
-        return Left(Failure(
-            409, response.message ?? "we have biz error logic from API side"));
+        if (response.status == ApiInternalStatus.SUCCESS) // success
+        {
+          // return data (success)
+          // return right
+          return Right(response.toDomain());
+        } else {
+          // return biz logic error
+          // return left
+          return Left(Failure(response.status ?? ApiInternalStatus.FAILURE,
+              response.message ?? ResponseMessage.DEFAULT));
+        }
+      } catch (error) {
+        return (Left(ErrorHandler.handle(error).failure));
       }
     } else {
       // return connection error
-      return Left(Failure(501, "please check your internet connection"));
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
     }
   }
 }
