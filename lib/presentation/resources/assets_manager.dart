a96dadee05dbeef68370c8ffde2a068afbb8dc31
Sout Rahim <sout.rahim@gmail.com>
2023-09-08 22:24:58 +0700

    state renderer

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -1,4 +1,5 @@
 const String IMAGE_PATH = "assets/images";
+const String JSON_PATH = "assets/json";
 
 class ImageAssets {
   static const String splashLogo = "$IMAGE_PATH/splash_logo.png";
@@ -11,4 +12,9 @@ class ImageAssets {
   static const String rightarrowIc = "$IMAGE_PATH/right_arrow_ic.svg";
   static const String solidCircleIc = "$IMAGE_PATH/solid_circle_ic.svg";
   static const String loginIc = "$IMAGE_PATH/login_ic.svg";
+}
+class JsonAssets {
+  static const String loading = "$JSON_PATH/loading.json";
+  static const String error = "$JSON_PATH/error.json";
+  static const String empty = "$JSON_PATH/empty.json";
 }
